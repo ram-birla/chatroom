741102336b65853232bef4524fcebd9f7b1d10e6
ram-birla <rambirla02@gmail.com>
2020-07-31 23:04:37 +0530

    host

--- task/views.py
+++ task/views.py
@@ -18,7 +18,7 @@ def accept(req):
         print(camera)
 
         ws = websocket.WebSocket()
-        ws.connect("ws://localhost:8000/ws/chat/python/")
+        ws.connect("ws://172.31.84.9:8000/ws/chat/python/")
         print("Sending")
         result = json.dumps(camera) 
   
