3cde1ed71ef6ffce8f9880c379c0c0e98ca789c0
ram-birla <rambirla02@gmail.com>
2020-08-01 07:38:10 +0530

    asgi

--- chatroom/asgi.py
+++ chatroom/asgi.py
@@ -6,6 +6,6 @@ import os
 import django
 from channels.routing import get_default_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_restapi.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatroom.settings")
 django.setup()
 application = get_default_application()
