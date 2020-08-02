d0ff67dd5b79b4fd593e721143ade70bda481a86
ram-birla <rambirla02@gmail.com>
2020-08-02 22:21:32 +0530

    final

--- chatroom/urls.py
+++ chatroom/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('chat/', include('chat.urls')),
     path('data/',include('task.urls')),
     path('admin/', admin.site.urls),
-    path('cht/',include('vivekchat.urls'))
 ]
